0b83d5b280ed160f6b23eca6d3d67518e2a3f300
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-11-08 23:40:15 +0530

    inheritance

---
+++ inheritance2.java
@@ -0,0 +1,16 @@
+public class inheritance2 {
+    
+}
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double initialBalance, double interestRate) {
+        super(accountNumber, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    public void applyInterest() {
+        double interest = getBalance() * interestRate / 100;
+        deposit(interest);
+    }
+}
